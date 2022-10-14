a6e7160b1887f341d43b9667f13d299033e9cda3
theresamm <mtheresamayo@gmail.com>
2022-10-14 09:49:21 +0000

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  transpileDependencies: true,
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: "all",
+    client: {
+      webSocketURL: {
+        port: process.env.GITPOD_WORKSPACE_ID ? 443 : undefined,
+      },
+    },
+}}
+module.exports = {
+  // options...
+  devServer: {
+      disableHostCheck: true
+  }
+}
